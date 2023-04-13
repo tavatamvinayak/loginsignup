a276ad32bab109612c42627904fa8576933cff17
vinayak tavatam <tavatamvinayak@gmail.com>
2023-04-14 00:02:00 +0530

    basic

--- src/Components/Simplelogin.js
+++ src/Components/Simplelogin.js
@@ -1,60 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react'
+
+function Simplelogin() {
 
 
-const Simplelogin = () => {
-    const [Fname, setFname] = useState('');
-    const [Email, setEmail] = useState('');
-    const [Password, setPassword] = useState('');
-    const [Form, setForm] = useState();
-
-
-    
-    // inputefieldValue onChange
-    const inputFieldValues =(e)=>{
-       
-        setForm({
-            ...Form, 
-            [e.target.name] : e.target.value
-        })
-    }
-  
-
-    // submit value 
-    const formValues = async(e)=>{
+    const inputField =(e)=>{
+        console.log(e.target.name , e.target.value)
+      }
+      const SendDataForm =(e)=>{
         e.preventDefault();
-
-       
-
-        const responese = await fetch('http://localhost:8080/' ,{
-            method: 'post',
-            body:JSON.stringify(Form)
-            ,headers:{
-                'Content-Type': 'application/json'
-            }
-        })
-        const data = await responese.json()
-        console.log(data)
-
-        useEffect(() => {
-        responese(data)
-    }, []);
-
-    }
     
-    return (
-        <>
-            <div>
-                <form onSubmit={formValues}>
-                    <input type="text"          name="Fname"             onChange={inputFieldValues} />
-                    <input type="email"         name="Email"            onChange={inputFieldValues} />
-                    <input type="password"      name="Password"      onChange={inputFieldValues} />
-                    <input type="submit"        value="submit" />
-                </form>
+    
+      }
 
-                <p>{JSON.stringify(Form)}</p>
-            </div>
-        </>
-    );
+  return (
+    <div>
+       <form action="" onSubmit={SendDataForm} method='post'>
+        <input type="email"   name="UserEmail"        onChange={inputField} placeholder='User Email' />
+        <input type="text"    name="UserPassword"     onChange={inputField} placeholder='User Password' />
+        <input type="submit"  value='submit' />
+      </form>
+    </div>
+  )
 }
 
-export default Simplelogin;
+export default Simplelogin
--- src/Components/Simplesignup.js
+++ src/Components/Simplesignup.js
@@ -1,34 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-const Simplesignup = () => {
-
-  
-    const [Email, setEmail] = useState('');
-    const [Password, setPassword] = useState('');
-
-    // inputefieldValue onChange
-    const inputFieldValues =()=>{
-        console.log(e.target.name  , e.target.value)
-    }
-
-    // submit value 
-    const formValues = ()=>{
-        set
-    }
+function Simplesignup() {
 
 
+  const inputField =(e)=>{
+    console.log(e.target.name , e.target.value)
+  }
+  const SendDataForm =(e)=>{
+    e.preventDefault();
 
 
+  }
   return (
-    <>
-    <form onSubmit={formValues}>
-        
-        <input type="email" name="email" value={Email} onChange={inputFieldValues} />
-        <input type="password" name="password" value={Password} onChange={inputFieldValues} />
-        <input type="submit" value="submit" />
-    </form> 
-    </>
-  );
+    <div>
+      <form action="" onSubmit={SendDataForm} method='post'>
+        <input type="text"    name="UserName"         onChange={inputField} placeholder='User Name' />
+        <input type="email"   name="UserEmail"        onChange={inputField} placeholder='User Email' />
+        <input type="text"    name="UserPassword"     onChange={inputField} placeholder='User Password' />
+        <input type="submit"  value='submit' />
+      </form>
+    </div>
+  )
 }
 
 export default Simplesignup;
